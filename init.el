4d981d5d8d661014b6769cfa1b7802bde1b48686
Brian Scaturro <scaturrob@gmail.com>
2016-12-03 15:31:17 -0500

    Adds missing packages and all-the-icons

--- init.el
+++ init.el
@@ -11,6 +11,9 @@
 
 (package-initialize)
 
+;; Adds an emacs vendor directory for manually placing .el files
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
 ;; Download the ELPA archive description if needed.
 ;; This informs Emacs about the latest versions of all packages, and
 ;; makes them available for download.
@@ -24,17 +27,19 @@
     clojure-mode-extra-font-locking
     cider
     company
+		dash
+		epl
+		pkg-info
+		queue
+		seq
+		spinner
     multiple-cursors
-    all-the-icons
     neotree))
 
 ;; Get all the packages!
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
-;; Adds an emacs vendor directory for manually placing .el files
-(add-to-list 'load-path "~/.emacs.d/vendor")
 
 ;; Adds /usr/local/bin to exec path so terminal can find my executables
 (setq exec-path (append exec-path '("/usr/local/bin")))
@@ -51,3 +56,21 @@
 (load "editing.el")
 (load "clojure.el")
 (load "files.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("68f7a53f5f1a8d30e5cd2d119fe6ecddb081bfe61bc427ca20eefd0abfada488" default)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (dash paredit neotree multiple-cursors company clojure-mode-extra-font-locking cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
