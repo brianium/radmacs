6b2c8e1ef1dcdc0d8ab472885ca0f057df6aef1b
Brian Scaturro <scaturrob@gmail.com>
2017-08-28 07:54:10 -0400

    Adds yaml support and multi-term

--- customizations/web.el
+++ customizations/web.el
@@ -1,5 +1,11 @@
+;; handles syntax for various web formats
+
+
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 4)
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
