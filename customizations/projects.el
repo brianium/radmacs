41835ca8479d3bb6b87b43a7c826a0672bf24740
Brian Scaturro <scaturrob@gmail.com>
2019-01-30 14:26:59 -0500

    Configure projectile for JS support

--- customizations/projects.el
+++ customizations/projects.el
@@ -1,4 +1,8 @@
-(projectile-global-mode)
-
+(require 'projectile)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
 (setq projectile-project-search-path '("~/projects/" "~/work/"))
 
+;; JS Projects
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
